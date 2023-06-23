9d9c8eb3e32957dffaaae402696587a5400a2443
NotHydra <rizky.irswanda115@gmail.com>
2023-06-23 11:14:35 +0700

    feat: group authentication

--- source/app.js
+++ source/app.js
@@ -2,11 +2,14 @@ const { Client, LocalAuth } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
 
 const { Dependency } = require("./dependency");
-const { default: mongoose } = require("mongoose");
+const { mongoose } = require("mongoose");
 const { Utility } = require("./utility");
 const { Command } = require("./command");
 
+const { GroupModel } = require("./model");
+
 const client = new Client({ authStrategy: new LocalAuth(), puppeteer: { args: ["--no-sandbox"] } });
+
 const dependency = new Dependency();
 const utility = new Utility();
 const command = new Command();
@@ -23,42 +26,56 @@ client.on("ready", async () => {
 client.on("message", async (message) => {
     const splittedMessage = message.body.split(" ");
 
-    if (utility.prefixIsValid(splittedMessage[0])) {
-        if ((await utility.isAdmin(message.author)) || (await utility.groupIsValid(message.from))) {
-            // if (splittedMessage.length == 1) {
-            //     await command.help(message);
-            // } else if (splittedMessage[1] == "everyone") {
-            //     await command.everyone(message, chat, client);
-            // } else if (splittedMessage[1] == "credit") {
-            //     await command.credit(message);
-            // } else if (splittedMessage[1] == "help") {
-            //     await command.help(message);
-            // } else if (splittedMessage[1] == "test" && dependency.adminArray.includes(message.author)) {
-            //     await command.test(message, chat, client);
-            // } else if (splittedMessage[1] == "group" && dependency.adminArray.includes(message.author)) {
-            //     if (splittedMessage[2] == "initialize") {
-            //         dependency.whitelistArray.push(message.from);
-            //         await message.reply("Bot Initialized");
-            //     } else if (splittedMessage[2] == "terminate") {
-            //         const whitelistRemoveIndex = dependency.whitelistArray.indexOf(message.from);
-            //         if (whitelistRemoveIndex != -1) {
-            //             dependency.whitelistArray.splice(whitelistRemoveIndex, 1);
-            //             await message.reply("Bot Terminated");
-            //         } else {
-            //             await message.reply("Bot Haven't Been Initialize Yet");
-            //         }
-            //     }
-            // }
+    if ((await utility.isAdmin(message.author)) || (await utility.groupIsValid(message.from))) {
+        if (await utility.prefixIsValid(message.from, splittedMessage[0])) {
+            if (splittedMessage.length == 1) {
+                await command.help(message);
+            } else if (splittedMessage[1] == "everyone") {
+                await command.everyone(message, client);
+            } else if (splittedMessage[1] == "credit") {
+                await command.credit(message);
+            } else if (splittedMessage[1] == "help") {
+                await command.help(message);
+            } else if (await utility.isAdmin(message.author)) {
+                if (splittedMessage[1] == "test") {
+                    await command.test(message, client);
+                } else if (splittedMessage[1] == "group") {
+                    if (splittedMessage[2] == "initialize") {
+                        const groupExist = await GroupModel.exists({ remote: message.from }).lean();
+                        if (groupExist == null) {
+                            await GroupModel.create({
+                                _id: await utility.latestModelId(GroupModel),
+                                remote: message.from,
+                                prefix: [],
+                                operator: [],
+                            });
+                            await message.reply("Bot Initialized");
+                        } else {
+                            await message.reply("Bot Already Initialized");
+                        }
+                    } else if (splittedMessage[2] == "terminate") {
+                        const groupExist = await GroupModel.exists({ remote: message.from }).lean();
+                        if (groupExist != null) {
+                            await GroupModel.deleteOne({ remote: message.from }).lean();
+                            await message.reply("Bot Terminated");
+                        } else {
+                            await message.reply("Bot Doesn't Exist");
+                        }
+                    }
+                }
+            }
         }
     }
 });
 
-// client.on("group_join", (notification) => {
-//     if (dependency.whitelistArray.includes(notification.chatId)) {
-//         if (notification.id.participant != dependency.botContact) {
-//             notification.reply("Selamat datang, mahasiswa baru jalur SNBP atau SNBT!");
-//         }
-//     }
-// });
+client.on("group_join", async (notification) => {
+    if (await utility.groupIsValid(notification.chatId)) {
+        if (notification.id.participant != dependency.botContact) {
+            notification.reply("Selamat datang!");
+        }
+    }
+
+    console.log();
+});
 
 client.initialize();
