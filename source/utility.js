0d82ec7fa1a939c20072510bb5b9cb127e8f43bb
NotHydra <rizky.irswanda115@gmail.com>
2023-06-23 11:09:13 +0700

    feat: group prefix

--- source/utility.js
+++ source/utility.js
@@ -6,9 +6,8 @@ const dependency = new Dependency();
 class Utility {
     constructor() {
         this.isAdmin = async (value) => {
-            const isExist = await AdminModel.exists({ contact: value });
+            const isExist = await AdminModel.exists({ contact: value }).lean();
 
-            console.log(isExist);
             if (isExist != null) {
                 return true;
             } else {
@@ -16,12 +15,18 @@ class Utility {
             }
         };
 
-        this.prefixIsValid = (value) => {
-            return dependency.prefixArray.includes(value);
+        this.prefixIsValid = async (remote, value) => {
+            const groupObject = await GroupModel.findOne({ remote: remote }).select({ prefix: 1 }).lean();
+
+            if (groupObject != null) {
+                return dependency.prefixArray.includes(value) || groupObject.prefix.includes(value);
+            } else {
+                return dependency.prefixArray.includes(value);
+            }
         };
 
         this.groupIsValid = async (value) => {
-            const isExist = await GroupModel.exists({ remote: value });
+            const isExist = await GroupModel.exists({ remote: value }).lean();
 
             if (isExist != null) {
                 return true;
