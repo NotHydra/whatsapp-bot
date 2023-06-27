8b857b90a5581eac80c02cddc3025c805b59c933
NotHydra <rizky.irswanda115@gmail.com>
2023-06-27 10:04:33 +0700

    feat: client initialize

--- source/app.ts
+++ source/app.ts
@@ -1,1 +1,14 @@
-console.log("Test");
+import { Client, LocalAuth } from "whatsapp-web.js";
+import qrcode from "qrcode-terminal";
+
+const client: Client = new Client({ authStrategy: new LocalAuth(), puppeteer: { args: ["--no-sandbox"] } });
+
+client.on("qr", (qr: string) => {
+    qrcode.generate(qr, { small: true });
+});
+
+client.on("ready", async (): Promise<void> => {
+    console.log("Bot Connected");
+});
+
+client.initialize();
