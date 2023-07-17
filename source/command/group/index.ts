0f17edb2677b9d591d3ed28c61c3e266346efcba
NotHydra <rizky.irswanda115@gmail.com>
2023-07-18 00:51:06 +0700

    feat: adjust group

--- source/command/group/index.ts
+++ source/command/group/index.ts
@@ -1,21 +1,21 @@
 import { Message } from "whatsapp-web.js";
+import { HydratedDocument } from "mongoose";
 
 import { latestModelId } from "../../utility";
 
 import { ModelIdInterface } from "../../common/interface/model";
+import { GroupRoleInterface } from "../../common/interface/model/group-role";
 
-import { GroupModel } from "../../model";
+import { GroupExtensionModel, GroupMessageModel, GroupModel, GroupOperatorModel, GroupPrefixModel, GroupRoleMemberModel, GroupRoleModel } from "../../model";
 
-export const groupInitialize = async (message: Message): Promise<void> => {
+export const groupInitialize = async (message: Message, name: string): Promise<void> => {
     const groupExist: ModelIdInterface = await GroupModel.exists({ remote: message.from }).lean();
     if (groupExist == null) {
         await GroupModel.create({
             _id: await latestModelId(GroupModel),
+            name: name,
             remote: message.from,
             active: true,
-            prefix: [],
-            operator: [],
-            message: [],
         });
 
         await message.reply("Bot Initialized");
@@ -27,7 +27,18 @@ export const groupInitialize = async (message: Message): Promise<void> => {
 export const groupTerminate = async (message: Message): Promise<void> => {
     const groupExist: ModelIdInterface = await GroupModel.exists({ remote: message.from }).lean();
     if (groupExist != null) {
+        const groupRoleArray: Array<HydratedDocument<GroupRoleInterface>> = await GroupRoleModel.find({ id_group: groupExist._id }).select({ _id: 1 }).lean();
+        groupRoleArray.forEach(async (groupRoleObject: HydratedDocument<GroupRoleInterface>): Promise<void> => {
+            await GroupRoleMemberModel.deleteMany({ id_group_role: groupRoleObject._id }).lean();
+        });
+
         await GroupModel.deleteOne({ remote: message.from }).lean();
+        await GroupOperatorModel.deleteMany({ id_group: groupExist._id }).lean();
+        await GroupPrefixModel.deleteMany({ id_group: groupExist._id }).lean();
+        await GroupRoleModel.deleteMany({ id_group: groupExist._id }).lean();
+        await GroupMessageModel.deleteMany({ id_group: groupExist._id }).lean();
+        await GroupExtensionModel.deleteMany({ id_group: groupExist._id }).lean();
+
         await message.reply("Bot Terminated");
     } else {
         await message.reply("Bot Doesn't Exist");
